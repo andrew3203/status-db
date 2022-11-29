ed567fd970073cdf52a7d039981a0ed7f16c5b0b
kuand <kuand3203@gmail.com>
2022-11-30 01:55:57 +0300

    add db

--- app/contact/admin.py
+++ app/contact/admin.py
@@ -1,3 +1,162 @@
 from django.contrib import admin
+from django.contrib.admin import SimpleListFilter
+from contact.models import *
 
-# Register your models here.
+
+class PhoneFilter(SimpleListFilter):
+    title = 'Телефон'
+    parameter_name = 'obj_phone'
+
+    def lookups(self, request, model_admin):
+        return [(1, 'Есть'), (0, 'Нет')]
+
+    def queryset(self, request, queryset):
+        if self.value() == 1:
+            return queryset.exclude(tel=None)
+        elif self.value() == 0:
+            return queryset.filter(tel=None)
+        else:
+            return queryset
+
+
+class EmailFilter(PhoneFilter):
+    title = 'Почта'
+    parameter_name = 'obj_mail'
+
+    def queryset(self, request, queryset):
+        if self.value() == 1:
+            return queryset.exclude(email=None)
+        elif self.value() == 0:
+            return queryset.filter(email=None)
+        else:
+            return queryset
+
+
+LIST_FDISPLAY_DEFAULT = [
+    'fio', 'tel', 'email', 'country', 'city', 'task',
+    'updated_at', 'created_at', 'whatsapp', 'telegram'
+]
+
+SEARCH_FIELDS_DEFAULT = [
+    'fio', 'tel', 'email', 'tel2', 'email2',
+    'country', 'city', 'addres', 'tg_username'
+]
+
+LIST_FILTER_DEFAULT = [
+    PhoneFilter, EmailFilter,
+    'task', 'telegram', 'whatsapp', 'country', 'city',
+]
+
+FIELDSETS_DEFAULT = [
+    ('Основное', {
+        'fields': (
+            ('fio', 'created_at'),
+            ('tel', 'email'),
+            ('tel2', 'email2'),
+            ('country', 'city', 'addres'),
+        ),
+    }),
+    ('', {
+        'fields': (
+            ('telegram', 'tg_username'),
+            ('whatsapp',),
+            ('task', 'updated_at')
+        ),
+    }),
+]
+READONLY_FIELD_DEFAULT = ('created_at', 'updated_at')
+
+
+@admin.register(RequestContact)
+class RequestContactAdmin(admin.ModelAdmin):
+    list_display = LIST_FDISPLAY_DEFAULT
+    search_fields = SEARCH_FIELDS_DEFAULT
+    list_filter = LIST_FILTER_DEFAULT
+
+    fieldsets = FIELDSETS_DEFAULT
+    readonly_fields = READONLY_FIELD_DEFAULT
+
+
+@admin.register(YaContact)
+class YaContactAdmin(admin.ModelAdmin):
+    list_display = LIST_FDISPLAY_DEFAULT + ['order_amount']
+    search_fields = SEARCH_FIELDS_DEFAULT
+    list_filter = LIST_FILTER_DEFAULT
+    
+    fieldsets = FIELDSETS_DEFAULT + \
+        [('', {
+            'fields': (
+                ('order_amount',),
+                ),
+            }
+        )]
+    readonly_fields = READONLY_FIELD_DEFAULT
+
+
+@admin.register(LinkedinContact)
+class LinkedinContactAdmin(admin.ModelAdmin):
+    list_display = LIST_FDISPLAY_DEFAULT + ['profile_link']
+    search_fields = SEARCH_FIELDS_DEFAULT
+    list_filter = LIST_FILTER_DEFAULT
+    
+    fieldsets = FIELDSETS_DEFAULT + \
+        [('', {
+            'fields': (
+                ('profile_link',),
+                ),
+            }
+        )]
+    readonly_fields = READONLY_FIELD_DEFAULT
+
+
+@admin.register(TsumContact)
+class TsumContactAdmin(admin.ModelAdmin):
+    list_display = LIST_FDISPLAY_DEFAULT + ['name']
+    search_fields = SEARCH_FIELDS_DEFAULT + ['name']
+    list_filter = LIST_FILTER_DEFAULT + ['name']
+    
+    fieldsets = FIELDSETS_DEFAULT + \
+        [('', {
+            'fields': (
+                ('name',),
+                ),
+            }
+        )]
+    readonly_fields = READONLY_FIELD_DEFAULT
+
+
+@admin.register(PropertyContact)
+class TsumContactAdmin(admin.ModelAdmin):
+    list_display = LIST_FDISPLAY_DEFAULT + ['company_field', 'name']
+    search_fields = SEARCH_FIELDS_DEFAULT + ['company_field', 'name']
+    list_filter = LIST_FILTER_DEFAULT + ['company_field', 'name']
+    
+    fieldsets = FIELDSETS_DEFAULT + \
+        [('', {
+            'fields': (
+                ('name', 'company_field',),
+                ),
+            }
+        )]
+    readonly_fields = READONLY_FIELD_DEFAULT
+
+
+@admin.register(VillageContact)
+class VillageContactAdmin(admin.ModelAdmin):
+    list_display = LIST_FDISPLAY_DEFAULT + ['name']
+    search_fields = SEARCH_FIELDS_DEFAULT + ['name']
+    list_filter = LIST_FILTER_DEFAULT + ['name']
+    
+    fieldsets = FIELDSETS_DEFAULT + \
+        [('', {
+            'fields': (
+                ('name',),
+                ),
+            }
+        )]
+    readonly_fields = READONLY_FIELD_DEFAULT
+
+
+admin.site.site_header = 'STATUS Админ панель'
+admin.site.site_title = 'Панель администратора STATUS'
+admin.site.index_title = 'Администратор'
--- app/contact/models.py
+++ app/contact/models.py
@@ -1,3 +1,166 @@
 from django.db import models
 
-# Create your models here.
+
+class TaskType(models.TextChoices):
+    MAILING = 'MAILING', 'Почта'
+    WH = 'WH', 'WhatsApp'
+    TG = 'Tg', 'Telegramm'
+    YANDEX = 'YANDEX', 'Яндекс'
+    FB = 'FB', 'Facebook'
+    INST = 'INST', 'Instagram'
+    GOOGLE = 'GOOGLE', 'Google'
+    
+
+class Contact(models.Model):
+    fio = models.CharField(
+        verbose_name = 'ФИО',
+        max_length=512, 
+        default='Пользователь', blank=True
+    )
+    tel = models.CharField(
+        verbose_name = 'Телефон',
+        max_length=20,
+        default=None, blank=True, null=True
+    )
+    email = models.CharField(
+        verbose_name = 'Почта',
+        max_length=50,
+        default=None, blank=True, null=True
+    )
+    tel2 = models.CharField(
+        verbose_name = 'Доп. Телефон',
+        max_length=20,
+        default=None, blank=True, null=True
+    )
+    email2 = models.CharField(
+        verbose_name = 'Доп. Почта',
+        max_length=20,
+        default=None, blank=True, null=True
+    )
+
+    telegram = models.BooleanField(
+        verbose_name = 'Есть в TG',
+        default=False
+    )
+    tg_username = models.CharField(
+        verbose_name = 'Телеграм',
+        max_length=20,
+        default=None, blank=True, null=True
+    )
+
+    whatsapp = models.BooleanField(
+        verbose_name = 'Есть в WH',
+        default=False
+    )
+
+    country = models.CharField(
+        verbose_name = 'Страна',
+        max_length=50,
+        default=None, blank=True, null=True
+    )
+    city = models.CharField(
+        verbose_name = 'Город',
+        max_length=512,
+        default=None, blank=True, null=True
+    )
+    addres = models.CharField(
+        verbose_name = 'Аддерс',
+        max_length=512,
+        default=None, blank=True, null=True
+    )
+
+    created_at = models.DateTimeField(
+        'Добавлен',
+        auto_now_add=True
+    )
+    updated_at = models.DateTimeField(
+        'Изменен',
+        auto_now=True, 
+    )
+    task = models.CharField(
+        'Задача',
+        max_length=25,
+        choices=TaskType.choices,
+        default=None, blank=True, null=True
+    )
+
+    class Meta:
+        abstract = True
+
+    def __str__(self) -> str:
+        return self.fio
+
+
+class RequestContact(Contact):
+    
+    class Meta:
+        verbose_name = 'Контакт'
+        verbose_name_plural = 'База заявок'
+
+
+class BrockerContact(Contact):
+    notice = models.TextField(
+        verbose_name = 'Заметка',
+        max_length=512
+    )
+
+    class Meta:
+        verbose_name = 'Контакт'
+        verbose_name_plural = 'Брокерские базы'
+
+
+class YaContact(Contact):
+    order_amount = models.FloatField(
+        verbose_name = 'Сумма заказов',
+        default=0, blank=True, null=True
+    )
+
+    class Meta:
+        verbose_name = 'Контакт'
+        verbose_name_plural = 'База Яндекс'
+
+class LinkedinContact(Contact):
+    profile_link = models.CharField(
+        verbose_name = 'Профиль',
+        max_length=512
+    )
+
+    class Meta:
+        verbose_name = 'Контакт'
+        verbose_name_plural = 'База Linkedin'
+
+
+class TsumContact(Contact):
+    name = models.CharField(
+        verbose_name = 'Название бренда',
+        max_length=512
+    )
+
+    class Meta:
+        verbose_name = 'Контакт'
+        verbose_name_plural = 'База ЦУМ'
+
+class PropertyContact(Contact):
+    name = models.CharField(
+        verbose_name = 'Название ЖК',
+        max_length=512
+    )
+    company_field = models.CharField(
+        verbose_name = 'Деятельность',
+        max_length=512
+    )
+
+    class Meta: 
+        verbose_name = 'Контакт'
+        verbose_name_plural = 'База ЖК'
+
+
+class VillageContact(Contact):
+    name = models.CharField(
+        verbose_name = 'Название поселка',
+        max_length=512
+    )
+    class Meta:
+        verbose_name = 'Контакт'
+        verbose_name_plural = 'База Загород'
+
